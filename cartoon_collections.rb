7c93027e09cfe83f197dabd6b8830f6d61508bfc
Agnes Park <agnespark824@gmail.com>
2020-01-24 06:21:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find {|index| index.
+  array.select {|index| index.
 end
 
 def find_the_cheese# code an argument here
