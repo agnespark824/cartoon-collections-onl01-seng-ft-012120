e58387f72fe87609dc893b2b98cb85b38cbcde77
Agnes Park <agnespark824@gmail.com>
2020-01-24 06:36:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect(cheese_types[0] || cheese_types[1] || cheese_types[2]) array.detect {|index| 
+  array.detect {|i| array.include?(cheese_types[0] || cheese_types[1] || cheese_types[2])}
 end
